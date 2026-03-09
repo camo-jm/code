f02048b5f978d85b3de2259583ac5cd8670d3782
camo-jm <joaquinsmateos2@gmail.com>
2026-03-09 18:43:48 -0300

    update

--- python/pwgen/pwgen.py
+++ python/pwgen/pwgen.py
@@ -1,11 +1,6 @@
 # this is basically an attempt at homemade encryption lollllllll
-import os
 import secrets
 
-keyword = str(input("enter your keyword"))
-
-true_keyword = ""
-for n in keyword:
-    true_keyword += str(ord(n))
-
-os.urandom()
+keyword = str(input("enter your keyword: "))
+password = secrets.token_hex( len(keyword) ) #huzzah
+print(password)
